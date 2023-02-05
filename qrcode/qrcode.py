d233077b843c51f482009020b211679c2a2c8c42
Salem Malick mohamed <64892800+saloum45@users.noreply.github.com>
2023-02-05 14:50:25 +0000

    Update qrcode.py

--- qrcode/qrcode.py
+++ qrcode/qrcode.py
@@ -1,8 +1,8 @@
 import pyqrcode
 from pyqrcode import QRCode
-ssid="FUNBOX FALL"
+ssid="put your wifi password"
 security="WPA2"
-password="Matarfatou76"
+password="put your password"
 
 qr=pyqrcode.create(f'WIFI:S:{ssid};T:{security};P:{password};;')
 qr.png('FALLBOX_QR_CODE.png', scale=6, module_color='#954567')
